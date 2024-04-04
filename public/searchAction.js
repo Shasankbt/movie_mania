875634cef13c5253f8ceb480a3e6dc1d7d5c9a5e
shasankbt <shasank1809@gmail.com>
2024-04-04 22:04:31 +0530

    added css for the top bar - no functionality

--- public/searchAction.js
+++ public/searchAction.js
@@ -1,51 +1,51 @@
-const searchInput = document.querySelector(".search-input")
-const searchOutput = document.querySelector("#search-output-id")
-const searchBarDiv = document.querySelector(".search-bar-div")
-const movieCardSearchTemplate = document.querySelector("[movie-card-search-template]")
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.querySelector(".search-input")
+    const searchOutput = document.querySelector("#search-output-id")
+    const searchBarDiv = document.querySelector(".search-bar-div")
+    const movieCardSearchTemplate = document.querySelector("[movie-card-search-template]")
+    let movies = null
+    
+    fetch('top100.json')
+        .then(res => res.json())
+        .then(data => {movies = data})
 
-console.log(movieCardSearchTemplate)
+    searchBarDiv.addEventListener("input" , e=>{
+        const input = e.target.value;
 
-let movies = null
-fetch('top100.json')
-    .then(res => res.json())
-    .then(data => {movies = data})
+        // to remove the previous cards for the fresh ones 
+        for (let i = searchOutput.children.length - 1; i >= 0; i--) {
+            const child = searchOutput.children[i];
+                searchOutput.removeChild(child);        
+        }
+        if(input != ""){
+            movies.forEach(movie => {
+                if(movie.Title.toLowerCase().includes(input.toLowerCase())){
+                    const card = movieCardSearchTemplate.content.cloneNode(true).children[0];
+                    
+                    const poster = card.querySelector('[poster]')
+                    const title = card.querySelector('[title]')
 
-searchBarDiv.addEventListener("input" , e=>{
-    const input = e.target.value;
+                    poster.src = "images/" + movie.imdbID + ".jpg";
+                    title.innerHTML = movie.Title + "<br><span style='opacity : 0.5; font-weight : 500 ; font-size : 1rem;'>" + movie.Year + "</span>";
 
-    // to remove the previous cards for the fresh ones 
-    for (let i = searchOutput.children.length - 1; i >= 0; i--) {
-        const child = searchOutput.children[i];
-            searchOutput.removeChild(child);        
-    }
-    if(input != ""){
-        movies.forEach(movie => {
-            if(movie.Title.toLowerCase().includes(input.toLowerCase())){
-                const card = movieCardSearchTemplate.content.cloneNode(true).children[0];
+                    card.addEventListener("click" , () => {
+                        window.location.href = "/id=" + movie.Title;
+                    })
+
+                    searchOutput.appendChild(card)
+                }
                 
-                const poster = card.querySelector('[poster]')
-                const title = card.querySelector('[title]')
+            })
+            searchOutput.lastChild.style.borderBottomRightRadius = "1rem";
+            searchOutput.lastChild.style.borderBottomLeftRadius = "1rem";
 
-                poster.src = "images/" + movie.imdbID + ".jpg";
-                title.innerHTML = movie.Title + "<br><span style='opacity : 0.5; font-weight : 500 ; font-size : 1rem;'>" + movie.Year + "</span>";
-
-                card.addEventListener("click" , () => {
-                    window.location.href = "/id=" + movie.Title;
-                })
-
-                searchOutput.appendChild(card)
-            }
-            
-        })
-        searchOutput.lastChild.style.borderBottomRightRadius = "1rem";
-        searchOutput.lastChild.style.borderBottomLeftRadius = "1rem";
-
-        // searchInput.style.borderBottomRightRadius = "0rem";
-        // searchInput.style.borderBottomLeftRadius = "0rem";
-        // searchBarDiv.style.height = "10rem";
-    }
-    else{
-        // searchBarDiv.style.height = "2rem";
-        // searchInput.style.borderRadius = "1rem";
-    }
+            // searchInput.style.borderBottomRightRadius = "0rem";
+            // searchInput.style.borderBottomLeftRadius = "0rem";
+            // searchBarDiv.style.height = "10rem";
+        }
+        else{
+            // searchBarDiv.style.height = "2rem";
+            // searchInput.style.borderRadius = "1rem";
+        }
+    })
 })
---
+++ public/topBar.js
@@ -0,0 +1,7 @@
+import "./searchAction.js"
+
+document.addEventListener("DOMContentLoaded", () =>{
+    document.querySelector(".logo").addEventListener("click", () =>{
+        window.location.href = "/"
+    })
+})
